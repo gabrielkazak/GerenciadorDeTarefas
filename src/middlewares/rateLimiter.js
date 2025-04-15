9513792d3cf6e333dfc0ac83a14d0d5685f61e7e
gabrielkazak <142548161+gabrielkazak@users.noreply.github.com>
2025-04-15 10:41:13 -0300

    Update rateLimiter.js

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 5 minutos
-  max: 100, // máximo de 5 requisições por IP nessa janela
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // máximo de 100 requisições por IP nessa janela
   message: 'Muitas tentativas. Tente novamente em 15 minutos.',
 });
 
